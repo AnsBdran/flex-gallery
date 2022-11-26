2bbdeb282f763912f2e772d30a91d2c64f21afb7
Anas Badran <ansaccts@gmail.com>
2022-11-26 20:22:11 +0200

    re uploading the project

--- index.js
+++
@@ -1,5 +0,0 @@
-const panels = document.querySelectorAll(".panels > *");
-panels.forEach((panel) =>
-  panel.addEventListener("click", () => toggleActive(panel))
-);
-const toggleActive = (panel) => panel.classList.toggle("active");
